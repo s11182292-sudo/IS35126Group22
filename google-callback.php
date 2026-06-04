ce0cfcb510c89415b24addb84a172e4a99d4c454
s11182292-sudo <s11182292@student.usp.ac.fj>
2026-06-04 17:17:00 +1200

    Update google-callback.php

--- google-callback.php
+++ google-callback.php
@@ -1,7 +1,7 @@
 <?php
 date_default_timezone_set('Pacific/Fiji');
-require 'session.php';
-require 'google-config.php';
+require __DIR__ . '/session.php';
+require __DIR__ . '/google-config.php';
 
 if (!isset($_GET['code'])) {
     die("No authorization code received.");
