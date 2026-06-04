9b4196110dad1a5d8f1959df6cfee37c70d35f97
s11182292-sudo <s11182292@student.usp.ac.fj>
2026-06-04 16:18:27 +1200

    Update process_booking.php

--- process_booking.php
+++ process_booking.php
@@ -1,9 +1,9 @@
 <?php
 
-require '../config/session.php';
-require '../config/auth_check.php';
-require '../config/role_check.php';
-require '../config/db.php';
+require 'session.php';
+require 'auth_check.php';
+require 'role_check.php';
+require 'db.php';
 
 requireRole('agent');
 
@@ -111,7 +111,7 @@ rel="stylesheet">
 
 <nav class="navbar navbar-dark bg-dark px-3">
     <span class="navbar-brand">🛫 Agent Booking Panel</span>
-    <a href="../auth/logout.php" class="btn btn-danger btn-sm">Logout</a>
+    <a href="logout.php" class="btn btn-danger btn-sm">Logout</a>
 </nav>
 
 <div class="container mt-4">
