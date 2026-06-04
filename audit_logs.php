6975b3ab4ef1ca1d59f3bbd59c501e1c9052bf77
s11182292-sudo <s11182292@student.usp.ac.fj>
2026-06-04 16:31:30 +1200

    Update audit_logs.php

--- audit_logs.php
+++ audit_logs.php
@@ -1,10 +1,10 @@
 <?php
 
-require '../session.php';
-require '../security.php';
-require '../auth_check.php';
-require '../role_check.php';
-require '../db.php';
+require 'session.php';
+require 'security.php';
+require 'auth_check.php';
+require 'role_check.php';
+require 'db.php';
 
 requireRole('admin');
 
@@ -27,7 +27,7 @@ $logs = $pdo->query("
 
 <nav class="navbar navbar-dark bg-dark px-3">
     <span class="navbar-brand">Audit Logs</span>
-    <a href="../logout.php" class="btn btn-danger">Logout</a>
+    <a href="logout.php" class="btn btn-danger">Logout</a>
 </nav>
 
 <div class="container mt-4">
