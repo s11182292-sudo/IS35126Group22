ed84aecc4e7e4e34262bc157f7c5a8c2403492be
s11182292-sudo <s11182292@student.usp.ac.fj>
2026-06-04 16:40:12 +1200

    Update logout.php

--- logout.php
+++ logout.php
@@ -1,7 +1,7 @@
 <?php
 date_default_timezone_set('Pacific/Fiji');
-require '../session.php';
-require '../db.php';
+require 'session.php';
+require 'db.php';
 
 /*
 |--------------------------------------------------------------------------
@@ -60,5 +60,5 @@ if (ini_get("session.use_cookies")) {
 | REDIRECT TO LOGIN
 |--------------------------------------------------------------------------
 */
-header("Location: ../login.php");
+header("Location: login.php");
 exit();
