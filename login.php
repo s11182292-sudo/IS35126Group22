7366337139266df62fb40fc27a16856f6f3e8fd5
s11182292-sudo <s11182292@student.usp.ac.fj>
2026-06-04 16:39:38 +1200

    Update login.php

--- login.php
+++ login.php
@@ -1,7 +1,7 @@
 <?php
 date_default_timezone_set('Pacific/Fiji');
-require '../session.php';
-require '../db.php';
+require 'session.php';
+require 'db.php';
 require '../vendor/autoload.php';
 
 use PHPMailer\PHPMailer\PHPMailer;
